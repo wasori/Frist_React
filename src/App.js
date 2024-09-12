cbed1e29565f5fffb9318e99d1a5d10d5e4a187e
wasori <wasori@naver.com>
2024-09-12 18:55:29 +0900

    state문법

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  
+  function 함수() {
+    console.log(1);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'red' , fontSize : '35px'} }>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> { 따봉 }</h4>
+        <p>2월 1일 발행</p>
+      </div>
+      <div className="list"> 
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 1일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 1일 발행</p>
+      </div>
+      <button onClick={()=>{글제목변경(['여자 코트 추천','강남 우동맛집','파이썬 독학'])}}>변경</button>
     </div>
   );
 }
