04ab002299b6b7557fb84186af523e76ca20667e
wasori <wasori@naver.com>
2024-09-23 18:00:47 +0900

    컴포넌트

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(0);
   
   function 함수() {
     console.log(1);
@@ -28,12 +29,38 @@ function App() {
         <p>2월 1일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{setModal(1)}} >{ 글제목[2] }</h4>
         <p>2월 1일 발행</p>
       </div>
-      <button onClick={()=>{글제목변경(['여자 코트 추천','강남 우동맛집','파이썬 독학'])}}>변경</button>
+
+      {
+        modal == 1 ? <Modal/> : null
+      }
+      
+
+      {/* <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+        }}>변경</button>
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>가나다순 정렬</button> */}
+
     </div>
   );
+}
+
+function Modal(){
+  return(
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
