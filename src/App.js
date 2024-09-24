8a741ebed04b8cde9553af92ebcc512d338cac26
wasori <wasori@naver.com>
2024-09-24 19:07:39 +0900

    map

--- src/App.js
+++ src/App.js
@@ -8,19 +8,15 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동맛집','파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
-  let [modal, setModal] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
+  let [modal, setModal] = useState(false);
   
-  function 함수() {
-    console.log(1);
-  }
-
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className="list">
+      {/* <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> { 따봉 }</h4>
         <p>2월 1일 발행</p>
       </div>
@@ -29,12 +25,30 @@ function App() {
         <p>2월 1일 발행</p>
       </div>
       <div className="list">
-        <h4 onClick={()=>{setModal(1)}} >{ 글제목[2] }</h4>
+        <h4 onClick={()=>{setModal(!modal)}} >{ 글제목[2] }</h4>
         <p>2월 1일 발행</p>
-      </div>
+      </div> */}
 
       {
-        modal == 1 ? <Modal/> : null
+        글제목.map(function(a,i){
+          return (
+            <div className="list"> 
+              <h4 onClick={()=>{setModal(!modal)}}>
+                { 글제목[i] }
+                <span onClick={()=>{ 
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy)
+                }}>👍</span> { 따봉[i] }
+              </h4>
+              <p>2월 1일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal == true ? <Modal/> : null
       }
       
 
